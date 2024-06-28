6d1dfc0e214dfec79315231eb9735ab18112e1f6
Ronak501 <ronaktalaviya51@gmail.com>
2024-06-28 22:56:17 +0530

    add react vite & react folder

---
+++ basic-vite/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+
+  return (
+    <h1>Ronak's World</h1>
+  )
+}
+
+export default App
---
+++ basic/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Ronak's World</h1>
+  );
+}
+
+export default App;
