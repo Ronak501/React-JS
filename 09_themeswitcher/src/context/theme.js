a63fd994907efe598615a893f27d523c6bf0dd7c
Ronak501 <ronaktalaviya51@gmail.com>
2024-07-05 20:38:14 +0530

    add theme Changer project

---
+++ 09_themeswitcher/src/context/theme.js
@@ -0,0 +1,13 @@
+import { createContext, useContext, UseContext } from 'react'
+
+export const ThemeContext = createContext({
+    themeMode: "light",
+    darkTheme: () => {},
+    lightTheme: () => {},
+})
+
+export const ThemeProvider = ThemeContext.Provider
+
+export default function useTheme(){
+    return useContext(ThemeContext)
+}
