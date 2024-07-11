3c2cd4369065e089f28bfa3f6f03180319b2337a
Ronak501 <ronaktalaviya51@gmail.com>
2024-07-11 19:17:49 +0530

    add mega project setup

---
+++ 12_megaBlog/src/config/config.js
@@ -0,0 +1,9 @@
+const conf = {
+  appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteProjectId : String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteDatabaseId : String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteCollectionId : String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteBucketId : String(import.meta.env.VITE_APPWRITE_URL)
+}
+
+export default conf
