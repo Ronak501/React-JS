42af5079b3d4d6375e53ef64cc102cbff666a263
Ronak501 <ronaktalaviya51@gmail.com>
2024-07-11 22:58:17 +0530

    add auth file in mega project

---
+++ 12_megaBlog/src/appwrite/auth.js
@@ -0,0 +1,56 @@
+import conf from "../config/config";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService{
+    client = new Client();
+    account;
+
+    constructor() {
+            this.client
+                .setEndpoint(conf.appwriteUrl)
+                .setProject(conf.appwriteProjectId);
+            this.account = new Account(this.client);
+    }
+
+    async creatAccount({email,password,name}){
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);  
+            if (userAccount){
+                // call another method
+                return this.login({email, password});
+            }
+            else return userAccount;
+        } catch (error) {
+            console.error('Error creating account:', error);
+        }  
+    }
+
+    async login({email, password}){
+        try {
+            return await this.account.createEmailPasswordSession(email, password);
+        } catch (error) {
+            console.error('Login Error:', error);
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("AppWrite Service : getCurrentUser:: Error",error);
+        }
+        return null;
+    }
+
+    async logout(){
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("AppWrite Service : logout:: Error",error);
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService;
